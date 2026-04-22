bf63285729d1f0b160301b18bf01439a89779c5d
David Akaluzia <jacck@iphone.iphone.com>
2026-04-22 14:28:47 -0400

    feat: Explicitly binded app to 0.0.0.0
    Explicitly binded the app to 0.0.0.0, so it doesn't default to localhost, but
    accept incoming traffic from any of is interface, while o its designated port.

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ app.get('/', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`UNIDEL Voting System: Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`SU Voting System: Server running on port ${PORT}`);
 });
+
